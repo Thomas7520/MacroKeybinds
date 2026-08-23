88a282c2c691f841bc5cd0dc718f72dfbfeef67d
Thomas7520 <thomasgerard1406@gmail.com>
2026-08-23 19:15:44 +0100

    Fix wheel icon buttons and click was not working

--- common/src/main/java/com/thomas7520/macrokeybinds/gui/wheel/WheelWidget.java
+++ common/src/main/java/com/thomas7520/macrokeybinds/gui/wheel/WheelWidget.java
@@ -491,11 +491,14 @@ public class WheelWidget extends AbstractWidget implements ContainerEventHandler
         public boolean mouseClicked(double mouseX, double mouseY, int button) {
             IMacro macro = findMacro(wheelSlot);
 
-            if(!clickEnabled || macro == null || !macro.isEnable()) {
+            if(!clickEnabled || button != 0 || !isMouseOver(mouseX, mouseY)
+                    || macro == null || !macro.isEnable()) {
                 return false;
             }
 
-            return super.mouseClicked(mouseX, mouseY, button);
+            playDownSound(Minecraft.getInstance().getSoundManager());
+            onClick(mouseX, mouseY);
+            return true;
         }
 
         @Override
